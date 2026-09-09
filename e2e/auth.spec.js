92ba9633bc3c1a119a7195841098482568c96931
seannez <seanneznansky@example.com>
2026-09-09 11:33:33 +0300

    added auth test

--- e2e/auth.spec.js
+++ e2e/auth.spec.js
@@ -6,3 +6,33 @@ test("login page is displayed", async ({ page }) => {
 
   await expect(page).toHaveURL(/login/);
 });
+
+test("unauthenticated user is redirected to login", async ({ page }) => {
+  await page.goto("/incidents");
+
+  await expect(page).toHaveURL(/\/login$/);
+  await expect(page.getByRole("button", { name: /Sign in/i })).toBeVisible();
+});
+
+test("check invalid email and password", async ({page}) =>{
+    await page.goto('/login')
+    await page.getByLabel("Email").fill("wrong@example.com")
+    await page.getByLabel("Password").fill("password")
+
+    await page.getByRole("button", { name: /sign in/i }).click();
+
+    await expect(
+        page.getByText(/Failed to login/i)
+    ).toBeVisible();
+})
+
+test("user can login successfully", async ({ page }) => {
+  await page.goto("/login");
+
+  await page.getByLabel("Email").fill("shon@example.com");
+  await page.getByLabel("Password").fill("mypassword");
+
+  await page.getByRole("button", { name: /sign in/i }).click();
+
+  await expect(page).toHaveURL(/\/incidents$/);
+});
